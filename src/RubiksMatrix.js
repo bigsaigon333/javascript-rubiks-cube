5a85249bcf60b311f6685eeca90f5c471277bf30
bigsaigon333 <likepeppermint@gmail.com>
2020-12-11 11:09:02 +0900

    feat: 각 명령어에 해당하는 동작 구현

--- src/RubiksMatrix.js
+++ src/RubiksMatrix.js
@@ -5,6 +5,8 @@ export default class RubiksMatrix {
       ["G", "C", "W"],
       ["G", "B", "B"]
     ];
+    this.matrixRowLength = this.matrix.length;
+    this.matrixColumnLength = this.matrix[0].length;
 
   }
 
@@ -17,6 +19,64 @@ export default class RubiksMatrix {
   }
   
   executeAlongCommand(command) {
-    // TODO: 정해진 동작을 실현!
+    if (command === "U" ) this.shiftColumn(0);
+    if (command === "U'") this.unshiftColumn(0);
+    if (command === "R" ) this.shiftRow(2);
+    if (command === "R'") this.unshiftRow(2);
+    if (command === "L" ) this.unshiftRow(0);
+    if (command === "L'") this.shiftRow(0);
+    if (command === "B" ) this.unshiftColumn(2);
+    if (command === "B'") this.shiftColumn(2);
+  }
+
+  shiftRow(col) {
+    let ri = 0;
+
+    while (ri < this.matrixRowLength - 1) {
+      this.swap({ row: ri, col }, { row: ri + 1, col });
+
+      ri++;
+    }
+  }
+
+  unshiftRow(col) {
+    let ri = this.matrixRowLength - 1;
+
+    while (ri > 0) {
+      this.swap({ row: ri, col }, { row: ri -1, col });
+      
+      ri--;
+    }
+  }
+
+  shiftColumn(row) {
+    let ci = 0;
+
+    while (ci < this.matrixColumnLength - 1) {
+      this.swap({ row, col: ci }, { row, col: ci + 1 });
+      
+      ci++;
+    }
+  }
+
+  unshiftColumn(row) {
+    let ci = this.matrixColumnLength - 1;
+    
+    while (ci > 0) {
+      this.swap({ row, col: ci }, { row, col: ci - 1 });
+      
+      ci--;
+    }
+  }
+
+  swap({ row: rowA, col: colA }, { row: rowB, col: colB }) {
+    [
+      this.matrix[rowA][colA],
+      this.matrix[rowB][colB]
+    ] =
+      [
+        this.matrix[rowB][colB],
+        this.matrix[rowA][colA]
+      ];
   }
 }
