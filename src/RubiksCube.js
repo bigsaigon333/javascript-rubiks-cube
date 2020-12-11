66a0bd71ad4939c59cf043be38a841ea88674c6b
bigsaigon333 <likepeppermint@gmail.com>
2020-12-12 01:50:58 +0900

    feat: 큐브의 조작법인 각 명령어에 해당하는 동작을 구현

--- src/RubiksCube.js
+++ src/RubiksCube.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 import RubiksCubeFace from "./RubiksCubeFace.js";
 
 export default class RubiksCube {
@@ -10,9 +11,63 @@ export default class RubiksCube {
       B: new RubiksCubeFace("Y"),
       D: new RubiksCubeFace("R"),
     };
+
+    this.setAdjacentPart();
+  }
+  
+  // eslint-disable-next-line max-lines-per-function
+  setAdjacentPart() {
+    const { U, D, L, R, F, B } = this.faces;
+
+    /*
+          U
+      L - F - R - B
+          D
+    */
+    F.setAdjacentPartSetter([U.setRow(2), R.setColumn(0), D.setRow(0), L.setColumn(2)]);
+    F.setAdjacentPartGetter([U.getRow(2), R.getColumn(0), D.getRow(0), L.getColumn(2)]);
+    
+    /*
+          U
+      R - B - L - F
+          D
+    */
+    B.setAdjacentPartSetter([U.setRow(0), L.setColumn(0), D.setRow(2), R.setColumn(2)]);
+    B.setAdjacentPartGetter([U.getRow(0), L.getColumn(0), D.getRow(2), R.getColumn(2)]);
+
+    /*
+          B
+      L - U - R - D
+          F
+    */
+    U.setAdjacentPartSetter([B.setRow(0), R.setRow(0), F.setRow(0), L.setRow(0)]);
+    U.setAdjacentPartGetter([B.getRow(0), R.getRow(0), F.getRow(0), L.getRow(0)]);
+
+    /*
+          B
+      R - D - L - U
+          F
+    */
+    D.setAdjacentPartSetter([B.setRow(2), L.setRow(2), F.setRow(2), R.setRow(2)]);
+    D.setAdjacentPartGetter([B.getRow(2), L.getRow(2), F.getRow(2), R.getRow(2)]);
+    
+    /*
+          U
+      B - L - F - R
+          D
+    */
+    L.setAdjacentPartSetter([U.setColumn(0), F.setColumn(0), D.setColumn(0), B.setColumn(2)]);
+    L.setAdjacentPartGetter([U.getColumn(0), F.getColumn(0), D.getColumn(0), B.getColumn(2)]);
+
+    /*
+          U
+      F - R - B - L
+          D
+    */
+    R.setAdjacentPartSetter([U.setColumn(2), B.setColumn(0), D.setColumn(2), F.setColumn(2)]);
+    R.setAdjacentPartGetter([U.getColumn(2), B.getColumn(0), D.getColumn(2), F.getColumn(2)]);
   }
 
-  // eslint-disable-next-line max-lines-per-function
   printCurrentState() {
     const { U, L, F, R, B, D } = this.faces;
 
@@ -45,7 +100,26 @@ export default class RubiksCube {
     }
   }
 
-  executeAlongCommand(command) {
-    //TODO: 각 명령어에 해당하는 동작을 실행
+  executeAlongCommand(plainCommand) {
+    const { singleCommand, isClockwise, isTwice } = this.parseCommand(plainCommand);
+
+    if (isTwice) { 
+      this.executeSingleCommand({ singleCommand, isClockwise });
+      this.executeSingleCommand({ singleCommand, isClockwise });
+    } else {
+      this.executeSingleCommand({ singleCommand, isClockwise });
+    }    
+  }
+
+  parseCommand(plainCommand) {
+    const [singleCommand] = plainCommand.split("");
+    const isClockwise = !plainCommand.includes("'");
+    const isTwice = plainCommand.includes("2");    
+    
+    return { singleCommand, isClockwise, isTwice };
+  }
+
+  executeSingleCommand({ singleCommand, isClockwise }) {
+    this.faces[singleCommand].rotate(isClockwise);
   }
 }
--- src/RubiksCubeFace.js
+++ src/RubiksCubeFace.js
@@ -11,27 +11,55 @@ export default class RubiksCubeFace {
     return this.face.map(col => col.join(" ")).join("\n");
   }
 
-  setColumn(row, dataArray) {
-    dataArray.forEach((data, colIndex) => {
-      this.face[row][colIndex] = data;
-    });
+  setColumn(col) {
+    return dataArray => {
+      dataArray.forEach((data, rowIndex) => this.face[rowIndex][col] = data);
+    };
   }
 
-  setRow(col, dataArray) {
-    dataArray.forEach((data, colIndex) => {
-      this.face[colIndex][col] = data;
-    });
+  setRow(row) {
+    return dataArray => {
+      dataArray.forEach((data, colIndex) => this.face[row][colIndex] = data);
+    };
   }
 
   getColumn(col) {
-    return this.face.map(row => row.find((data, colIndex) => colIndex === col));
+    return () => this.face.map(row => row.find((data, colIndex) => colIndex === col));
   }
 
   getRow(row) {
-    return [...this.face[row]];
+    return () => [...this.face[row]];
   }
 
   getArray() {
     return JSON.parse(JSON.stringify(this.face));
+  }
+
+  rotate(isClockwise) {
+    const data = this.adjacentGetter.map(getter => getter());
+    const nextData = isClockwise ? this.unshift(data) : this.shift(data);
+
+    this.adjacentSetter.map((setter, index) => setter(nextData[index]));
+  }
+
+  shift(array) {
+    const [first, ...rest] = array;
+
+    return [...rest, first];
+  }
+
+  unshift(array) {
+    const [last] = array.slice(-1);
+    const rest = array.slice(0, -1);
+
+    return [last, ...rest];
+  }
+
+  setAdjacentPartSetter(setterArray) {
+    this.adjacentSetter = setterArray;
+  }
+
+  setAdjacentPartGetter(getterArray) {
+    this.adjacentGetter = getterArray;
   }
 }
