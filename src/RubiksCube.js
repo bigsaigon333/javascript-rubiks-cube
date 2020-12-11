1efc6ae88c122ab49cf333c08e5ac1528f42a087
bigsaigon333 <likepeppermint@gmail.com>
2020-12-12 01:46:09 +0900

    refactor: 루빅스 큐브의 현재 상태를 Flat view 방식으로 출력하도록 수정

--- src/RubiksCube.js
+++ src/RubiksCube.js
@@ -3,30 +3,25 @@ import RubiksCubeFace from "./RubiksCubeFace.js";
 export default class RubiksCube {
   constructor() {
     this.faces = {
-      U: new RubiksCubeFace("W"),
-      D: new RubiksCubeFace("B"),
-      L: new RubiksCubeFace("G"),
-      R: new RubiksCubeFace("Y"),
+      U: new RubiksCubeFace("B"),
+      L: new RubiksCubeFace("W"),
       F: new RubiksCubeFace("O"),
-      B: new RubiksCubeFace("R")
+      R: new RubiksCubeFace("G"),
+      B: new RubiksCubeFace("Y"),
+      D: new RubiksCubeFace("R"),
     };
   }
 
   // eslint-disable-next-line max-lines-per-function
   printCurrentState() {
     const { U, L, F, R, B, D } = this.faces;
-    const ROW_STRING_LENGTH = U.getRow(0).join(" ").length;
-    const SPACE_BETWEEN_FACES = " ".repeat(ROW_STRING_LENGTH);
 
     // Top Section
     this.printFaceAtCenter(U);
     console.log("");
 
     // Middle Section
-    for (let ri = 0; ri < 3; ri++) {
-      const line = [L, F, R, B].map(face => face.getRow(ri).join(" ")).join(SPACE_BETWEEN_FACES);
-      console.log(line);      
-    }
+    this.printFaces([L, F, R, B]);
     console.log("");
 
     // Bottom Section
@@ -36,8 +31,18 @@ export default class RubiksCube {
 
   printFaceAtCenter(face) {
     face.getArray().map(row => row.join(" "))
-      .map(rowString => rowString.padStart(rowString.length * 4, " "))
+      .map(rowString => rowString.padStart(rowString.length * 3, " "))
       .forEach(text => console.log(text));
+  }
+
+  printFaces(faces) {
+    const ROW_STRING_LENGTH = 5;
+    const SPACE_BETWEEN_FACES = " ".repeat(ROW_STRING_LENGTH);
+
+    for (let ri = 0; ri < 3; ri++) {
+      const line = faces.map(face => face.getRow(ri)().join(" ")).join(SPACE_BETWEEN_FACES);
+      console.log(line);      
+    }
   }
 
   executeAlongCommand(command) {
