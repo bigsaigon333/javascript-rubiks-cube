cc0ada07c19dacbc5faac5fea144800c4089d0f1
bigsaigon333 <likepeppermint@gmail.com>
2020-12-12 21:02:41 +0900

    refactor: 함수의 최대 길이 조정 및 불필요한 함수, 변수 삭제

--- src/RubiksCube.js
+++ src/RubiksCube.js
@@ -12,60 +12,90 @@ export default class RubiksCube {
       B: new RubiksCubeFace("Y"),
       D: new RubiksCubeFace("R"),
     };
-    this.countExecutedCommands = 0;
 
     this.setAdjacentPart();
   }
   
   // eslint-disable-next-line max-lines-per-function
   setAdjacentPart() {
-    const { U, D, L, R, F, B } = this.faces;
+    this.setFAdjacentPart();
+    this.setBAdjacentPart();
 
-    /*
-          U
-      L - F - R - B
-          D
-    */
+    this.setUAdjacentPart();
+    this.setDAdjacentPart();
+
+    this.setLAdjacentPart();
+    this.setRAdjacentPart();
+  }
+
+  /*
+        U
+    L - F - R - B
+        D
+  */
+  setFAdjacentPart() {
+    const { F, U, R, D, L } = this.faces;
+
     F.setAdjacentPartSetter([U.setRow(2), R.setColumn(0), D.setRow(0), L.setColumn(2)]);
     F.setAdjacentPartGetter([U.getRow(2), R.getColumn(0), D.getRow(0), L.getColumn(2)]);
-    
-    /*
-          U
-      R - B - L - F
-          D
-    */
+  }
+  
+  /*
+        U
+    R - B - L - F
+        D
+  */
+  setBAdjacentPart() {
+    const { B, U, L, D, R } = this.faces;
+
     B.setAdjacentPartSetter([U.setRow(0), L.setColumn(0), D.setRow(2), R.setColumn(2)]);
     B.setAdjacentPartGetter([U.getRow(0), L.getColumn(0), D.getRow(2), R.getColumn(2)]);
+  }
 
-    /*
-          B
-      L - U - R - D
-          F
-    */
+  /*
+        B
+    L - U - R - D
+        F
+  */
+  setUAdjacentPart() {
+    const { U, B, R, F, L } = this.faces;
+
     U.setAdjacentPartSetter([B.setRow(0), R.setRow(0), F.setRow(0), L.setRow(0)]);
-    U.setAdjacentPartGetter([B.getRow(0), R.getRow(0), F.getRow(0), L.getRow(0)]);
+    U.setAdjacentPartGetter([B.getRow(0), R.getRow(0), F.getRow(0), L.getRow(0)]);    
+  }
 
-    /*
-          B
-      R - D - L - U
-          F
-    */
+  /*
+        B
+    R - D - L - U
+        F
+  */
+  setDAdjacentPart() {
+    const { D, L, R, F, B } = this.faces;
+
     D.setAdjacentPartSetter([B.setRow(2), L.setRow(2), F.setRow(2), R.setRow(2)]);
-    D.setAdjacentPartGetter([B.getRow(2), L.getRow(2), F.getRow(2), R.getRow(2)]);
-    
-    /*
-          U
-      B - L - F - R
-          D
-    */
+    D.setAdjacentPartGetter([B.getRow(2), L.getRow(2), F.getRow(2), R.getRow(2)]);  
+  }
+
+  /*
+       U
+   B - L - F - R
+       D
+ */
+  setLAdjacentPart() {
+    const { L, U, F, D, B } = this.faces;
+
     L.setAdjacentPartSetter([U.setColumn(0), F.setColumn(0), D.setColumn(0), B.setColumn(2)]);
     L.setAdjacentPartGetter([U.getColumn(0), F.getColumn(0), D.getColumn(0), B.getColumn(2)]);
+  }
 
-    /*
-          U
-      F - R - B - L
-          D
-    */
+  /*
+        U
+    F - R - B - L
+        D
+  */
+  setRAdjacentPart() {
+    const { R, U, B, D, F } = this.faces;
+
     R.setAdjacentPartSetter([U.setColumn(2), B.setColumn(0), D.setColumn(2), F.setColumn(2)]);
     R.setAdjacentPartGetter([U.getColumn(2), B.getColumn(0), D.getColumn(2), F.getColumn(2)]);
   }
@@ -103,36 +133,20 @@ export default class RubiksCube {
   }
 
   executeAlongCommand(plainCommand) {
-    const { singleCommand, isClockwise, isTwice } = this.parseCommand(plainCommand);
+    const { singleCommand, isClockwise } = this.parseCommand(plainCommand);
 
-    if (isTwice) { 
-      this.executeSingleCommand({ singleCommand, isClockwise });
-      this.executeSingleCommand({ singleCommand, isClockwise });
-    } else {
-      this.executeSingleCommand({ singleCommand, isClockwise });
-    }    
-  }
-
-  parseCommand(plainCommand) {
-    const [singleCommand] = plainCommand.split("");
-    const isClockwise = !plainCommand.includes("'");
-    const isTwice = plainCommand.includes("2");    
-    
-    return { singleCommand, isClockwise, isTwice };
-  }
-
-  executeSingleCommand({ singleCommand, isClockwise }) {
     if (singleCommand === "S") {
       this.scrambleCube();
     } else {
       this.executeRotationCommand({ singleCommand, isClockwise });
     }
-    
-    this.countExecutedCommands++;
   }
 
-  executeRotationCommand({ singleCommand, isClockwise }) {
-    this.faces[singleCommand].rotate(isClockwise);
+  parseCommand(plainCommand) {
+    const [singleCommand] = plainCommand.split("");
+    const isClockwise = !plainCommand.includes("'");
+    
+    return { singleCommand, isClockwise };
   }
 
   scrambleCube() {
@@ -140,12 +154,11 @@ export default class RubiksCube {
     
     for (let i = 0; i < NUM_RANDOM_ROTATION_COMMAND; i++) {
       const randomRotationCommand = getRandomRotationCommand();
-      const { singleCommand: singleRandomCommand, isClockwise } = this.parseCommand(randomRotationCommand);
-      this.executeRotationCommand({ singleCommand: singleRandomCommand, isClockwise });
+      this.executeRotationCommand(this.parseCommand(randomRotationCommand));
     }
   }
 
-  getCountExecutedCommands() {
-    return this.countExecutedCommands;
+  executeRotationCommand({ singleCommand, isClockwise }) {
+    this.faces[singleCommand].rotate(isClockwise);
   }
 }
